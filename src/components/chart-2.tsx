6c4cb8f6ceab2450a11b849223ee38dd59b36e1d
xuhao <bingoxuhao@gmail.com>
2021-10-24 15:46:18 +0800

    制作 footer

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -11,30 +11,64 @@ export const Chart2 = () => {
     myChart.setOption(createEchartOptions({
       xAxis: {
         type: 'value',
-        boundaryGap: [0, 0.01]
+        boundaryGap: [0, 0.01],
+        splitLine: {show: false},
+        axisLabel: {show: false}
       },
       yAxis: {
+        axisTick: {show: false},
         type: 'category',
-        data: ['Brazil', 'Indonesia', 'USA', 'India', 'China', 'World'],
+        data: ['城关公安局', '七里河公安局', '西固区公安局', '安宁区公安局', '红古区公安局', '永登县公安局', '皋兰县公安局', '榆中县公安局', '新区公安局'],
+        axisLabel: {
+          formatter(val){
+            return val.replace('公安局', '\n公安局')
+          }
+        }
       },
       series: [
         {
-          name: '破案排名 1',
+          name: '2020年',
           type: 'bar',
-          data: [18203, 23489, 29034, 104970, 131744, 630230]
+          data: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+          itemStyle: {
+            normal: {
+              color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+                offset: 0,
+                color: '#2034f9'
+              },{
+                offset: 1,
+                color: '#04a1ff'
+              }])
+            }
+          }
         },
         {
-          name: '破案排名 2',
+          name: '2021年',
           type: 'bar',
-          data: [19325, 23438, 31000, 121594, 134141, 681807]
+          data: [2, 3, 4, 5, 6, 7, 8, 9, 10],
+          itemStyle: {
+            normal: {
+              color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+                offset: 0,
+                color: '#b92ae8'
+              }, {
+                offset: 1,
+                color: '#6773e7'
+              }])
+            }
+          }
         }
       ]
     }));
   }, [])
   return (
-    <div className="bordered 管辖统计">
+    <div className="bordered 破获排名">
       <h2>案件破获排名</h2>
       <div ref={divRef} className="chart"></div>
+      <div className="legend">
+        <span className="first" />破案排名1
+        <span className="second" />破案排名2
+      </div>
     </div>
   )
 }
