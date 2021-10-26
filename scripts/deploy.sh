985fb1a9e42fc510b4df59e4cbfee4b8771dd3d8
xuhao <bingoxuhao@gmail.com>
2021-10-26 22:31:09 +0800

    更新 deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,5 @@ git init &&
 git add . &&
 git commit -m deploy &&
 git remote add origin git@github.com:xuhaooo/big-screen-display-preview.git &&
-git remote add gitee git@gitee.com:bingoxuhao/big-screen-display-preview.git &&
 git push -uf origin master:gh-pages &&
-git push -uf gitee master:gt-pages &&
 cd -;
