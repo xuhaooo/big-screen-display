8495f88ade204d9ea068e547a5f8f025b77fa9d5
xuhao <bingoxuhao@gmail.com>
2021-10-27 21:17:37 +0800

    更新 deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
-yarn build &&
+yarn build --base ./ &&
 cd dist &&
 git init &&
 git add . &&
 git commit -m deploy &&
 git remote add origin git@github.com:xuhaooo/big-screen-display-preview.git &&
-git push -uf origin master:gh-pages &&
+git remote add gitee git@gitee.com:bingoxuhao/big-screen-display-preview.git &&
+git push -u -f origin master &&
+git push -f gitee master &&
 cd -;
