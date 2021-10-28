44ecd7b66518c23b57cf216af53f71f1eb41765e
xuhao <bingoxuhao@gmail.com>
2021-10-28 16:06:21 +0800

    new header.png

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './home.scss';
-import headerBg from '../images/header.png'
+import headerBg from '../images/headerbg.png'
 import {Chart1} from '../components/chart-1'
 import {Chart2} from '../components/chart-2'
 import {Chart3} from '../components/chart-3';
